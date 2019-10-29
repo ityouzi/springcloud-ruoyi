b0df3abbe186934eaf06a89e96705b654a95240a
lizhen <3428893330@qq.com>
2019-10-29 16:50:18 +0800

    模块构建

---
+++ ruoyi-service/ruoyi-system/src/main/java/com/ityouzi/system/SystemApplication.java
@@ -0,0 +1,22 @@
+package com.ityouzi.system;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+@EnableFeignClients
+@MapperScan("com.ityouzi.system.mapper")
+@Slf4j
+public class SystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class,args);
+        log.info("======>>启动系统程序");
+    }
+
+}
---
+++ ruoyi-service/ruoyi-system/src/main/java/com/ityouzi/system/mapper/SysOssMapper.java
@@ -0,0 +1,12 @@
+package com.ityouzi.system.mapper;
+
+
+import com.ityouzi.core.core.dao.BaseMapper;
+import com.ityouzi.system.domain.SysOss;
+
+/**
+ * 文件上传
+ */
+public interface SysOssMapper extends BaseMapper<SysOss>
+{
+}
